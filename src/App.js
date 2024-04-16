62e20628a47ddd23e353b0ece89d76d7542eefd0
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2024-04-17 04:47:36 +0500

    add: css and html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import TicketCard from './components/ticketCard/ticketCard';
 
 function App() {
+
+
+  const buttonListNumbersOne = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+
+  const buttonListNumbersTwo = [1, 2]
+
+
+
+
   return (
+
+
+
     <div className="App">
-      q
+
+
+      <TicketCard
+        buttonListNumbersOne={buttonListNumbersOne}
+        buttonListNumbersTwo={buttonListNumbersTwo}
+
+      />
+
+
+
+
     </div>
   );
 }
